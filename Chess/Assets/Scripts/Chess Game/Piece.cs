84d782fdbe8691cda42f567f91948f0dd34d24f1
Coobbyo <61762261+Coobbyo@users.noreply.github.com>
2022-11-09 13:46:11 -0700

    Finished Part 2
    https://www.youtube.com/watch?v=8W-MJlgAz8U

--- Chess/Assets/Scripts/Chess Game/Piece.cs
+++ Chess/Assets/Scripts/Chess Game/Piece.cs
@@ -27,8 +27,6 @@ public abstract class Piece : MonoBehaviour
 
     public void SetMaterial(Material material)
     {
-        //if(materialSetter == null)
-            //materialSetter = GetComponent<MaterialSetter>();
         materialSetter.SetSingleMaterial(material);
     }
 
@@ -44,7 +42,10 @@ public abstract class Piece : MonoBehaviour
 
     public virtual void MovePiece(Vector2Int coords)
     {
-
+        Vector3 targetPosition = board.CalculatePositionFromCoords(coords);
+        occupiedSquare = coords;
+        hasMoved = true;
+        tweener.MoveTo(transform, targetPosition);
     }
 
     protected void TryToAddMove(Vector2Int coords)
